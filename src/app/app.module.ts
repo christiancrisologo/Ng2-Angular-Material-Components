b4d737dcc2102020927d2b4b72daac492958110c
Christian Crisologo <Christian.Crisologo@gecogaming.com>
2017-03-30 18:32:57 +1100

    init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,32 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { MaterialModule } from '@angular/material';
 import { AppComponent } from './app.component';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
+import { SliderComponent } from './components/slider/slider.component';
+import { ButtonComponent } from './components/button/button.component';
+import { RadiobuttonComponent } from './components/radiobutton/radiobutton.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { MenuComponent } from './components/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidenavComponent,
+    SidenavComponent,
+    SliderComponent,
+    ButtonComponent,
+    RadiobuttonComponent,
+    ToolbarComponent,
+    MenuComponent,
+    ToolbarComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
